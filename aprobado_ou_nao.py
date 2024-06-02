d0df9529890019aa5eaf40a8f1246dd881a7f429
egber <egber22@gmail.com>
2024-06-02 17:00:46 -0300

    primeiro

---
+++ aprobado_ou_nao.py
@@ -0,0 +1,9 @@
+#recibir nota del alumno
+nota = int(input("Digite la nota: "))
+if nota >= 60:
+    #si (if) la nota fue mayor que 60
+    print("Aprobado")
+#si no
+else:
+    #si la nota fue menor que 60
+    print("Reprobado")
